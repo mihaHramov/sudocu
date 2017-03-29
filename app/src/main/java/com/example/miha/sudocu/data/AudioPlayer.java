e73b81ff34c43cd85dce292dc541595e89388c25
miha <vadim.hramov.69@mail.ru>
2017-03-29 19:24:29 +0300

    create AudioPlayer

--- app/src/main/java/com/example/miha/sudocu/data/AudioPlayer.java
+++ app/src/main/java/com/example/miha/sudocu/data/AudioPlayer.java
@@ -11,8 +11,9 @@ import com.example.miha.sudocu.R;
 public class AudioPlayer {
     private MediaPlayer mPlayer;
     private Context mContext;
-    public void stop(){
-        if(mPlayer!=null){
+
+    public void stop() {
+        if (mPlayer != null) {
             mPlayer.release();
             mPlayer = null;
         }
@@ -22,7 +23,7 @@ public class AudioPlayer {
         mContext = context;
     }
 
-    public void play(int resId){
+    public void play(int resId) {
         stop();
         mPlayer = MediaPlayer.create(mContext, resId);
         mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
