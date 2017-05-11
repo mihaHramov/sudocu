aad6b4ebb47ece61a2ab2e16ec02ddbe12d63457
miha <vadim.hramov.69@mail.ru>
2017-05-11 15:02:10 +0300

    add service for play music in background

---
+++ app/src/main/java/com/example/miha/sudocu/presenter/Service/MyMediaPlayerService.java
@@ -0,0 +1,34 @@
+package com.example.miha.sudocu.presenter.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.example.miha.sudocu.MainActivity;
+import com.example.miha.sudocu.data.AudioPlayer;
+
+
+
+public class MyMediaPlayerService extends Service {
+    private AudioPlayer mPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mPlayer = AudioPlayer.getInstance(this);
+        int resultMelody = intent.getIntExtra(MainActivity.myMediaPlayer, 0);
+        mPlayer.play(resultMelody);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
