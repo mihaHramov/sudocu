9d1a76f3c86b21058fc429ae946d6c23ed1ccdba
miha <vadim.hramov.69@mail.ru>
2017-05-12 17:32:31 +0300

    add toolbar and "AlertDialog" to "MenuActivity"

--- app/src/main/java/com/example/miha/sudocu/View/MenuActivity.java
+++ app/src/main/java/com/example/miha/sudocu/View/MenuActivity.java
@@ -1,52 +1,16 @@
 package com.example.miha.sudocu.View;
 
-import android.app.Dialog;
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
-import com.example.miha.sudocu.MainActivity;
 import com.example.miha.sudocu.R;
+import com.example.miha.sudocu.presenter.Adapter.AlertDialog;
 
-public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+public class MenuActivity extends Activity implements View.OnClickListener {
 
-    public static final String SETTINGS = "MenuActivitySettingsComplexity";
-    private TextView dialogComplexityTextValue;
-    private SeekBar dialogComplexitySeekbar;
-    private Dialog dialog;
-    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-
-
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            int level;
-            if (progress < 20) {
-                level = R.string.level_easy;
-                if(level<1) {
-                    level = 1;
-                }
-            } else if (progress < 45) {
-                level = R.string.level_medium;
-            } else {
-                level = R.string.level_hard;
-            }
-
-            dialogComplexityTextValue.setText(getResources().getString(level));
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-
-        }
-    };
+    private AlertDialog dialog;
 
     @Override
     public void onClick(View v) {
@@ -57,13 +21,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(i);
                 break;
             case R.id.newGame:
-                dialog.show();
-                break;
-            case R.id.newGameWithSettings:
-                dialog.hide();
-                i = new Intent(this, MainActivity.class);
-                i.putExtra(this.SETTINGS,dialogComplexitySeekbar.getProgress());
-                startActivity(i);
+                dialog.showDialog();
                 break;
         }
     }
@@ -72,17 +30,8 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        initDialog();
+        dialog = new AlertDialog(this);
         findViewById(R.id.restore).setOnClickListener(this);
         findViewById(R.id.newGame).setOnClickListener(this);
-    }
-
-    private void initDialog() {
-        dialog = new Dialog(this);
-        dialog.setContentView(R.layout.dialog_settings_view);
-        dialogComplexityTextValue = (TextView) dialog.findViewById(R.id.textTitleValue);
-        dialogComplexitySeekbar = (SeekBar) dialog.findViewById(R.id.complexity_grid);
-        dialogComplexitySeekbar.setOnSeekBarChangeListener(seekBarChangeListener);
-        dialog.findViewById(R.id.newGameWithSettings).setOnClickListener(this);
     }
 }
