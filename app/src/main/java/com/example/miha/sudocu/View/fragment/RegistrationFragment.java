56a9bc6f1673ffc5aa6123359a41725a832aa695
miha <vadim.hramov.69@mail.ru>
2017-05-21 02:10:08 +0300

    add fragment "RegistrationFragent"

---
+++ app/src/main/java/com/example/miha/sudocu/View/fragment/RegistrationFragment.java
@@ -0,0 +1,75 @@
+package com.example.miha.sudocu.View.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.miha.sudocu.R;
+import com.example.miha.sudocu.View.IView.IFragment;
+import com.example.miha.sudocu.presenter.IPresenter.IPresenterRegistration;
+import com.example.miha.sudocu.presenter.PresenterRegistrationFragment;
+
+
+public class RegistrationFragment extends Fragment implements View.OnClickListener, IFragment {
+    private Button auth;
+    private EditText login, password;
+    private IPresenterRegistration presenter;
+
+    private LoginCallback mCallbacks;
+
+
+    public interface LoginCallback {
+        void onLogin();
+    }
+
+    @Override
+    public void callbacks() {
+        mCallbacks.onLogin();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        presenter = new PresenterRegistrationFragment(this);
+        if (activity instanceof LoginCallback) {
+            mCallbacks = (LoginCallback) activity;
+        }
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView =
+                inflater.inflate(R.layout.registration_fragment, container, false);
+        auth = (Button) rootView.findViewById(R.id.auth);
+        password = (EditText) rootView.findViewById(R.id.password);
+        login = (EditText) rootView.findViewById(R.id.login);
+        auth.setOnClickListener(this);
+        return rootView;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.auth:
+                presenter.login(login.getText().toString(), password.getText().toString());
+                break;
+        }
+    }
+}
