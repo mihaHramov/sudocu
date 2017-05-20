32d5e3fefd270439ec4f40a9b0249b777050bf7a
miha <vadim.hramov.69@mail.ru>
2017-05-21 02:04:45 +0300

    implements interface  "IPresenterRecordsListFragment"

---
+++ app/src/main/java/com/example/miha/sudocu/presenter/PresenterRecordsListFragment.java
@@ -0,0 +1,44 @@
+package com.example.miha.sudocu.presenter;
+
+
+import android.app.Activity;
+import android.widget.ListView;
+
+import com.example.miha.sudocu.data.DP.ChallengeDP;
+import com.example.miha.sudocu.data.DP.ChallengeDpImpl;
+import com.example.miha.sudocu.data.model.Challenge;
+import com.example.miha.sudocu.presenter.Adapter.AdapterChallenge;
+import com.example.miha.sudocu.presenter.IPresenter.IPresenterOfFragment;
+
+import java.util.ArrayList;
+
+public class PresenterRecordsListFragment implements IPresenterOfFragment{
+    private AdapterChallenge adapter;
+    private ChallengeDP challengeDP = new ChallengeDpImpl();
+    private ChallengeDP.ChallengeDPCallbacks challengeDPCallbacks = new ChallengeDP.ChallengeDPCallbacks() {
+        @Override
+        public void onSuccess(ArrayList<Challenge> challenges) {
+            adapter.setData(challenges);
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onError() {
+
+        }
+    };
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void initListView(ListView listView) {
+        listView.setAdapter(adapter);
+    }
+    public PresenterRecordsListFragment(Activity activity){
+        adapter = new AdapterChallenge(activity);
+        challengeDP.getAllScore(challengeDPCallbacks);
+    }
+}
