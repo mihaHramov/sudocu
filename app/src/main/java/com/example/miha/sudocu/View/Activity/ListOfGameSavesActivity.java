ea2a377065d5dc0c89160b0f46d0d5fe5427a35a
miha <vadim.hramov.69@mail.ru>
2017-05-21 01:58:40 +0300

    add TabLayout to "ListOfGameSavesActivity"

---
+++ app/src/main/java/com/example/miha/sudocu/View/Activity/ListOfGameSavesActivity.java
@@ -0,0 +1,58 @@
+package com.example.miha.sudocu.View.Activity;
+
+import android.os.Bundle;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.miha.sudocu.R;
+import com.example.miha.sudocu.View.fragment.ListOfCompleteGameFragment;
+import com.example.miha.sudocu.View.fragment.ListOfGameFragment;
+import com.example.miha.sudocu.presenter.Adapter.AdapterLocalGameList;
+import com.example.miha.sudocu.presenter.Adapter.AlertDialog;
+
+
+public class ListOfGameSavesActivity extends FragmentActivity {
+    private AlertDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_game_saves);
+        initTabLayout();
+        initToolbar();
+        dialog = new AlertDialog(this);
+    }
+
+    public void initToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.list_game);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.navigateButtonBack:
+                        finish();
+                        break;
+                    case R.id.newGame:
+                        dialog.showDialog();
+                        break;
+                }
+
+                return false;
+            }
+        });
+    }
+    public void initTabLayout(){
+        AdapterLocalGameList adapterLocalGameList = new AdapterLocalGameList(getSupportFragmentManager());
+        adapterLocalGameList.addFragment(new ListOfGameFragment(),"first");
+        adapterLocalGameList.addFragment(new ListOfCompleteGameFragment(),"second");
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapterLocalGameList);
+        ((TabLayout )findViewById(R.id.tableLayout)).setupWithViewPager(viewPager);
+    }
+}
