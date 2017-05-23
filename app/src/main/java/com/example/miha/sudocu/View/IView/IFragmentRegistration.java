536fa1f026c824313faf11e66db2afa46b35ebf1
miha <vadim.hramov.69@mail.ru>
2017-05-23 22:31:24 +0300

    add interface "IFragmentRegistration"

--- app/src/main/java/com/example/miha/sudocu/View/IView/IFragmentRegistration.java
+++ app/src/main/java/com/example/miha/sudocu/View/IView/IFragmentRegistration.java
@@ -2,9 +2,12 @@ package com.example.miha.sudocu.View.IView;
 
 import android.app.Activity;
 
+import com.example.miha.sudocu.data.model.User;
 
-public interface IFragment {
+
+
+public interface IFragmentRegistration {
     void onAttach(Activity activity);
     void onDetach();
-    void callbacks();
+    void onLogin(User user);
 }
--- app/src/main/java/com/example/miha/sudocu/presenter/PresenterRegistrationFragment.java
+++ app/src/main/java/com/example/miha/sudocu/presenter/PresenterRegistrationFragment.java
@@ -1,11 +1,9 @@
 package com.example.miha.sudocu.presenter;
 
-
-import android.util.Log;
-
-import com.example.miha.sudocu.View.IView.IFragment;
+import com.example.miha.sudocu.View.IView.IFragmentRegistration;
 import com.example.miha.sudocu.data.DP.ILogin;
 import com.example.miha.sudocu.data.DP.Login;
+import com.example.miha.sudocu.data.model.User;
 import com.example.miha.sudocu.presenter.IPresenter.IPresenterRegistration;
 
 import java.util.Hashtable;
@@ -13,28 +11,30 @@ import java.util.Map;
 
 public class PresenterRegistrationFragment implements IPresenterRegistration {
     private ILogin loginInSystem = new Login();
-    private IFragment view;
+    private IFragmentRegistration view;
+
     private ILogin.OnLogin onLogin = new ILogin.OnLogin() {
         @Override
-        public void onLogin() {
-            view.callbacks();
+        public void onLogin(User user) {
+            if (user != null) {
+                view.onLogin(user);
+            }
         }
 
         @Override
         public void onError() {
-            Log.d("errorConnect","mama");
         }
     };
 
-    public  PresenterRegistrationFragment(IFragment fragment){
+    public PresenterRegistrationFragment(IFragmentRegistration fragment) {
         view = fragment;
     }
 
     @Override
-    public void login(String login,String password) {
-        Map<String,String> loginParams = new Hashtable<>();
-        loginParams.put("login",login);
-        loginParams.put("password",password);
-        loginInSystem.login(loginParams,onLogin);
+    public void login(String login, String password) {
+        Map<String, String> loginParams = new Hashtable<>();
+        loginParams.put("login", login);
+        loginParams.put("password", password);
+        loginInSystem.login(loginParams, onLogin);
     }
 }
