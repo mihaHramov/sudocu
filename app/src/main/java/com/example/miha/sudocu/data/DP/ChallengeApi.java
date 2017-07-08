76cedbba9b85897429d5064289de5c5d774a8de4
miha <vadim.hramov.69@mail.ru>
2017-07-08 19:27:12 +0300

    fix POST request

--- app/src/main/java/com/example/miha/sudocu/data/DP/ChallengeApi.java
+++ app/src/main/java/com/example/miha/sudocu/data/DP/ChallengeApi.java
@@ -2,25 +2,17 @@ package com.example.miha.sudocu.data.DP;
 
 
 import com.example.miha.sudocu.data.model.Challenge;
-import com.example.miha.sudocu.data.model.Grid;
-import com.example.miha.sudocu.data.model.MyClass;
 import com.example.miha.sudocu.data.model.User;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Map;
 
-import okhttp3.RequestBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
+
 import retrofit2.http.Field;
-import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 
 
@@ -33,9 +25,7 @@ public interface ChallengeApi {
     @GET("login")
     Call<User> login(@QueryMap Map<String, String> options);
 
-    /*    @POST("addChallenge")
-        Call<Object> addChallenge(@Body Grid first);
-    */
     @POST("addChallenge")
-    Call<MyClass> addChallenge(@Body  MyClass params);
+    @FormUrlEncoded
+    Call<Void> addChallenge(@Field("sendJson")  String options);
 }
--- app/src/main/java/com/example/miha/sudocu/data/DP/ChallengeDpImpl.java
+++ app/src/main/java/com/example/miha/sudocu/data/DP/ChallengeDpImpl.java
@@ -1,17 +1,11 @@
 package com.example.miha.sudocu.data.DP;
 
-import android.util.Log;
-
 import com.example.miha.sudocu.data.model.Challenge;
 import com.example.miha.sudocu.data.model.Grid;
-import com.example.miha.sudocu.data.model.MyClass;
 import com.example.miha.sudocu.data.model.User;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,35 +22,33 @@ public class ChallengeDpImpl implements ChallengeDP {
 
     @Override
     public void sendGame(User user, Grid grid, final ChallengeDPSendGameCallbacks callbacks) {
-        MyClass myClass = new MyClass();
-        myClass.setLast("last");
-        myClass.setFirst("first");
-        instance.addChallenge(myClass).enqueue(new Callback<MyClass>() {
+        Gson g = new Gson();
+        instance.addChallenge(g.toJson(grid)).enqueue(new Callback<Void>() {
             @Override
-            public void onResponse(Call<MyClass> call, Response<MyClass> response) {
+            public void onResponse(Call<Void> call, Response<Void> response) {
                 if (response.isSuccessful()) {
-                    Log.d("mihaHramov1", response.body().getLast());
+                    callbacks.onSuccess();
                 } else {
-                    Log.d("mihaHramov", "error");
+                    callbacks.onError("error");
                 }
             }
 
             @Override
-            public void onFailure(Call<MyClass> call, Throwable t) {
-                Log.d("mihaHramov", t.getMessage());
+            public void onFailure(Call<Void> call, Throwable t) {
+                 callbacks.onError(t.getMessage());
             }
         });
     }
 
     @Override
     public void getAllScore(final ChallengeDPGetAllScoreCallbacks callbacks) {
-        RetroClient.getInstance().getAllScore().enqueue(new Callback<ArrayList<Challenge>>() {
+        instance.getAllScore().enqueue(new Callback<ArrayList<Challenge>>() {
             @Override
             public void onResponse(Call<ArrayList<Challenge>> call, Response<ArrayList<Challenge>> response) {
                 if (response.isSuccessful()) {
                     callbacks.onSuccess(response.body());
                 } else {
-                    Log.d("error", "mihaSuccess");
+                    callbacks.onError();
                 }
             }
 
@@ -66,5 +58,4 @@ public class ChallengeDpImpl implements ChallengeDP {
             }
         });
     }
-
 }
