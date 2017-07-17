ec74ef0917ebf947a2b6ee87f1f1b104d20b1e74
miha <vadim.hramov.69@mail.ru>
2017-07-17 22:51:06 +0300

    add adapter to View

--- app/src/main/java/com/example/miha/sudocu/View/fragment/RecordsListFragment.java
+++ app/src/main/java/com/example/miha/sudocu/View/fragment/RecordsListFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.miha.sudocu.R;
+import com.example.miha.sudocu.data.DP.RetroClient;
 import com.example.miha.sudocu.presenter.Adapter.AdapterChallenge;
 import com.example.miha.sudocu.presenter.IPresenter.IPresenterOfFragment;
 import com.example.miha.sudocu.presenter.PresenterRecordsListFragment;
@@ -17,18 +18,21 @@ import com.example.miha.sudocu.presenter.PresenterRecordsListFragment;
 public class RecordsListFragment extends Fragment {
     private ListView listOfRecords;
     private IPresenterOfFragment presenter;
+    private AdapterChallenge adapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView =
                 inflater.inflate(R.layout.records_list_fragment, container, false);
         listOfRecords = (ListView) rootView.findViewById(R.id.list_records);
-        presenter.initListView(listOfRecords);
+        listOfRecords.setAdapter(adapter);
         return rootView;
     }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        presenter = new PresenterRecordsListFragment(new AdapterChallenge(getActivity()));
+        presenter = new PresenterRecordsListFragment(RetroClient.getInstance());
+        adapter = new AdapterChallenge(getContext());
     }
 }
