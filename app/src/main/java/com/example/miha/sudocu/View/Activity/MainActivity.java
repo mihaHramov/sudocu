65b8d895b50cf0b60014f246010f0efa67a8fadd
miha <vadim.hramov.69@mail.ru>
2017-07-17 22:50:30 +0300

    use ButterKnife

--- app/src/main/java/com/example/miha/sudocu/View/Activity/MainActivity.java
+++ app/src/main/java/com/example/miha/sudocu/View/Activity/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -25,8 +24,11 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends Activity implements IGridView, View.OnClickListener {
+
+public class MainActivity extends Activity implements IGridView {
     private IPresenterGrid presenterGrid;
     private TextView lastEditText;
     private boolean lastAnswer = true;
@@ -39,41 +41,56 @@ public class MainActivity extends Activity implements IGridView, View.OnClickLis
     public static String myMediaPlayer = "myMediaPlayer";
 
     private void initViews() {
-        for (int i = 0; i < arrIntIdGrid.length; i++) {
-            tableLayouts.put(arrIntIdGrid[i], (TableLayout) findViewById(arrIntIdGrid[i]));
+        ButterKnife.bind(this);
+        for (int anArrIntIdGrid : arrIntIdGrid) {
+            tableLayouts.put(anArrIntIdGrid, (TableLayout) findViewById(anArrIntIdGrid));
         }
-
-        int arrIntId[] = new int[]{R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9};
-        for (int i = 0; i < arrIntId.length; i++)
-            findViewById(arrIntId[i]).setOnClickListener(this);
         chronometer = (Chronometer) findViewById(R.id.chronometer);
 
     }
 
+    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9})
+    void clickOnButton(View v) {
+        String answer = ((Button) v).getText().toString();
+        if (lastEditText == null) return;
+        lastEditText.setText(answer);
+        presenterGrid.answer(answer);
+    }
 
     @Override
     public IGridView showGrid(String[][] grid) {
         countOfRowsAndCols = grid.length;
         for (int i = 0; i < countOfRowsAndCols; i++) {
             for (int j = 0; j < countOfRowsAndCols; j++) {
-                TextView text = (TextView) getLayoutInflater().inflate(R.layout.text_view, null);
+                TextView text = (TextView) getLayoutInflater().inflate(R.layout.text_view, null);//получил свою view
                 text.setId(i * countOfRowsAndCols + j);
                 text.setText(grid[i][j]);
                 if (grid[i][j].isEmpty()) {
-                    text.setOnClickListener(this);
+                    text.setOnClickListener(v -> {
+                        if (lastEditText != null) {
+                            if (lastAnswer) {
+                                lastEditText.setBackgroundColor(Color.WHITE);
+                            }
+                            lastAnswer = true;
+                        }
+                        lastEditText = (TextView) v;
+                        lastEditText.setText("");
+                        lastEditText.setBackgroundResource(R.drawable.back);
+                    });
                 }
                 int resI = (i / 3) * 3;
                 int resJ = j / 3;
                 TableLayout tab = tableLayouts.get(arrIntIdGrid[resI + resJ]);//выбрал необходимый квадрат
                 int rowsCol = tab.getChildCount();
                 if (tab.getChildCount() > 0) {
+                    TableRow row;
                     if (((TableRow) tab.getChildAt(rowsCol - 1)).getChildCount() > 2) {
-                        TableRow row = new TableRow(this);
-                        row.addView(text);
+                        row = new TableRow(this);
                         tab.addView(row);
                     } else {
-                        ((TableRow) tab.getChildAt(rowsCol - 1)).addView(text);
+                        row = ((TableRow) tab.getChildAt(rowsCol - 1));
                     }
+                    row.addView(text);
                 } else {
                     TableRow row = new TableRow(this);
                     row.addView(text);
@@ -91,7 +108,6 @@ public class MainActivity extends Activity implements IGridView, View.OnClickLis
         setContentView(R.layout.activity_main);
         initViews();
         toolbarInit();
-
         presenterGrid = new PresenterGrid(this);
         presenterGrid.init(savedInstanceState, this);
     }
@@ -123,21 +139,18 @@ public class MainActivity extends Activity implements IGridView, View.OnClickLis
     private void toolbarInit() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.main);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                int id = item.getItemId();
-                switch (id) {
-                    case R.id.navigateButtonBack:
-                        finish();
-                        break;
-                    case R.id.reloadGame:
-                        presenterGrid.reloadGame();
-                        break;
-                }
-
-                return false;
+        toolbar.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.navigateButtonBack:
+                    finish();
+                    break;
+                case R.id.reloadGame:
+                    presenterGrid.reloadGame();
+                    break;
             }
+
+            return false;
         });
     }
 
@@ -148,29 +161,6 @@ public class MainActivity extends Activity implements IGridView, View.OnClickLis
         presenterGrid.onSaveInstanceState(outState);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v instanceof Button) {
-            String answer = ((Button) v).getText().toString();
-            if (lastEditText == null) return;
-            lastEditText.setText(answer);
-            presenterGrid.answer(answer);
-            return;
-        }
-
-        if (v instanceof TextView) {
-            if (lastEditText != null) {
-                if (lastAnswer) {
-                    lastEditText.setBackgroundColor(Color.WHITE);
-                }
-                lastAnswer = true;
-            }
-            lastEditText = (TextView) v;
-            lastEditText.setText("");
-            lastEditText.setBackgroundResource(R.drawable.back);
-            return;
-        }
-    }
 
     @Override
     public void setGameTime(long time) {
@@ -229,6 +219,7 @@ public class MainActivity extends Activity implements IGridView, View.OnClickLis
         }, 3000);
 
     }
+
     public void playInBackground(int res) {
         startService(new Intent(this, MyMediaPlayerService.class).putExtra(myMediaPlayer, res));
     }
