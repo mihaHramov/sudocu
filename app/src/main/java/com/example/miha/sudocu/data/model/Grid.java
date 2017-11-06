0905e9a1e83b84862a044221beb9fb066bb09449
miha <vadim.hramov.69@mail.ru>
2017-11-06 10:14:19 +0200

    ingect IGenerateGame to Grid class

--- app/src/main/java/com/example/miha/sudocu/data/model/Grid.java
+++ app/src/main/java/com/example/miha/sudocu/data/model/Grid.java
@@ -1,6 +1,5 @@
 package com.example.miha.sudocu.data.model;
 
-
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Hashtable;
@@ -9,34 +8,39 @@ import java.util.Random;
 
 import javax.annotation.Generated;
 
+import com.example.miha.sudocu.data.DP.IGenerateGame;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 @Generated("org.jsonschema2pojo")
 public class Grid implements Serializable {
-
     private transient Random random = new Random();
+    private int razmer;
+
     @SerializedName("undefined")
     @Expose
     private int undefined;
+
     @SerializedName("gameTime")
     @Expose
     private long gameTime = 0;
+
     @SerializedName("id")
     @Expose
     private long id = 0;
+
     @SerializedName("name")
     @Expose
     private String name = "";
+
     @SerializedName("complexity")
     @Expose
     private int complexity;
-    private transient final int dlinaBloka = 3;
-    private String[] grid = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};// основное множество
-    private int razmer = grid.length;
+
     @SerializedName("pole")
     @Expose
-    private String[][] pole = new String[razmer][razmer];
+    private String[][] pole;
+
     @SerializedName("answers")
     @Expose
     private Map<Integer, String> answers = new Hashtable<>();
@@ -104,38 +108,27 @@ public class Grid implements Serializable {
         return undefined;
     }
 
-    public Boolean getAnswer(int i, String s) {
-        int str = i / razmer;
-        int col = i % razmer;
+    public Boolean getAnswer(int id, String s) {
+        int str = id / pole.length;
+        int col = id % pole.length;
         pole[str][col] = s;
-        //потом изменить
-        for (int k = 0; k < pole.length; k++) {
-            for (int j = 0; j < pole.length; j++) {
-                int key = k * pole.length + j;
-                if (answers.containsKey(key) && !answers.get(key).equals(pole[k][j])) {
-                    return false;//ошибка
-                }
+        for (Map.Entry<Integer, String> entry : answers.entrySet()) {
+            int i = entry.getKey() / pole.length;
+            int j = entry.getKey() % pole.length;
+            if (!entry.getValue().equals(pole[i][j])) {
+                return false;
             }
         }
         undefined = 0;
         return true;
     }
 
-    private void initAnswer() {
-        do {
-            int temp = random.nextInt((razmer - 1) * (razmer - 1));
-            if (answers.get(temp) != null) continue;
-            answers.put(temp, pole[temp / razmer][temp % razmer]);
-            pole[temp / razmer][temp % razmer] = "";
-        } while (answers.size() < undefined);
-    }
-
-
-    public Answer[][] getGameGrid(){
+    public Answer[][] getGameGrid() {
+        razmer = pole.length;
         Answer[][] answer = new Answer[razmer][razmer];
-        for (int i =0;i<razmer;i++){
-            for (int j =0;j<razmer;j++){
-                answer[i][j] = new Answer(pole[i][j],answers.containsKey(i*razmer+j));
+        for (int i = 0; i < razmer; i++) {
+            for (int j = 0; j < razmer; j++) {
+                answer[i][j] = new Answer(pole[i][j], answers.containsKey(i * razmer + j));
             }
         }
         return answer;
@@ -144,125 +137,10 @@ public class Grid implements Serializable {
         return pole;
     }
 
-    //Обмен двух строк в пределах одного района (swapRowsSmall)
-    private void swapRowsSmall() {
-        int rajon = random.nextInt(dlinaBloka) * dlinaBloka;
-        int stingOne = random.nextInt(dlinaBloka) + rajon;//1+0
-        int stringTwo = random.nextInt(dlinaBloka) + rajon;//2+0
-        for (int j = 0; j < razmer; j++) {
-            String temp = pole[stingOne][j];
-            pole[stingOne][j] = pole[stringTwo][j];
-            pole[stringTwo][j] = temp;
-        }
-    }
-
-    private void swapRowsArea() {
-        int rajonOne = random.nextInt(dlinaBloka) + 1; //рандомно выбрали район
-        int rajonTwo = random.nextInt(dlinaBloka) + 1; //рандомно выбрали второй
-
-        int rajonOneBottom = rajonOne * dlinaBloka;
-        int rajonOneTop = rajonOneBottom - dlinaBloka;
-        // нужно вычислить шаг
-        int shag = (rajonTwo * dlinaBloka - dlinaBloka) - rajonOneTop;
-
-        for (int i = rajonOneTop; i < rajonOneBottom; i++) {
-            for (int j = 0; j < razmer; j++) {
-                String temp = pole[i][j];
-                pole[i][j] = pole[i + shag][j];
-                pole[i + shag][j] = temp;
-            }
-        }
-    }
-
-    private void swapColumsArea() {
-        int rajonOne = random.nextInt(dlinaBloka) + 1; //рандомно выбрали район
-        int rajonTwo = random.nextInt(dlinaBloka) + 1;        //рандомно выбрали второй
-        int rajonOneRight = rajonOne * dlinaBloka;
-        int rajonOneLeft = rajonOneRight - dlinaBloka;//9-3=6
-
-        int rajonTwoRight = rajonTwo * dlinaBloka;//3*1=3
-        int rajonTwoLeft = rajonTwoRight - dlinaBloka;//3-3=0
-        int shag = rajonTwoLeft - rajonOneLeft;//0-6=-6
-
-        for (int i = 0; i < razmer; i++) {
-            for (int j = rajonOneLeft; j < rajonOneRight; j++) {
-                String temp = pole[i][j];
-                pole[i][j] = pole[i][j + shag];
-                pole[i][j + shag] = temp;
-            }
-        }
-
-    }
-
-    //объмен столбцов в
-    private void swapColumsSmall() {
-        int rajon = random.nextInt(dlinaBloka);//получил район
-        int leftBottom = rajon * dlinaBloka;//leftBottom 0*3 = 0
-        int stingOne = random.nextInt(dlinaBloka) + leftBottom;
-        //взять рандомно столбец
-        int stringTwo = random.nextInt(dlinaBloka) + leftBottom;
-        for (int i = 0; i < razmer; i++) {
-            String temp = pole[i][stingOne];
-            pole[i][stingOne] = pole[i][stringTwo];
-            pole[i][stringTwo] = temp;
-        }
-    }
-
-
-    private void transperentMatrix() {
-        for (int i = 0; i < razmer; i++) {
-            for (int j = i; j < razmer; j++) {
-                String temp = pole[j][i];
-                pole[j][i] = pole[i][j];
-                pole[i][j] = temp;
-            }
-        }
-    }
-
-
-    public Grid init() {
-        //главное сделать сначала первую часть (верхние 3 строки)
-        int sdvig = 0;
-        for (int i = 0; i < razmer / dlinaBloka; i++) {
-            int temp = sdvig, j = 0;
-            while (j < razmer) {
-                pole[i][j] = grid[temp];
-                j++;
-                temp++;
-                if (temp == razmer) {
-                    temp = 0;
-                }
-            }
-            sdvig += dlinaBloka;
-        }
-
-        sdvig = 1;
-        for (int i = dlinaBloka; i < razmer; i++) {
-            int temp = sdvig, j = 0;
-            while (j < razmer) {
-                pole[i][j] = pole[i - dlinaBloka][temp];//Arra[temp];
-                j++;
-                temp++;
-                if (temp == razmer) {
-                    temp = 0;
-                }
-            }
-        }
-        Random j = new Random();
-        int temp = j.nextInt(10) + 10;
-        for (int i = 0; i < temp; i++) {
-            int iter = j.nextInt(temp) + i;
-            do {
-                transperentMatrix();//готово
-                swapRowsSmall();//готовро
-                swapRowsArea();//готово
-                swapColumsSmall();//готово
-                swapColumsArea();
-                iter--;
-            }
-            while (iter > 0);
-        }
-        initAnswer();
+    public Grid init(IGenerateGame generateGame) {
+        pole = generateGame.generateGame();
+        razmer = pole.length;
+        answers = generateGame.initAnswer(getUndefined());
 
         long date = System.currentTimeMillis();
         SimpleDateFormat sdf = new SimpleDateFormat("MMM MM dd, yyyy h:mm a");
