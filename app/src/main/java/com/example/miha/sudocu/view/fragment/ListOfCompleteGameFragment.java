064debfbe79b1202dd81403a2ae4b45989e228fd
miha <vadim.hramov.69@mail.ru>
2017-11-06 10:20:10 +0200

    clear import

--- app/src/main/java/com/example/miha/sudocu/view/fragment/ListOfCompleteGameFragment.java
+++ app/src/main/java/com/example/miha/sudocu/view/fragment/ListOfCompleteGameFragment.java
@@ -1,6 +1,5 @@
 package com.example.miha.sudocu.view.fragment;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
