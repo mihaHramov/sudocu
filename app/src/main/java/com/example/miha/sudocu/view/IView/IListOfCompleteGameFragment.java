c1b178af86bc1307a5e4cd3b299597d3b599fcb6
miha <vadim.hramov.69@mail.ru>
2017-11-13 00:18:48 +0200

    add new method "deleteGameFromList"

--- app/src/main/java/com/example/miha/sudocu/view/IView/IListOfCompleteGameFragment.java
+++ app/src/main/java/com/example/miha/sudocu/view/IView/IListOfCompleteGameFragment.java
@@ -1,8 +1,14 @@
 package com.example.miha.sudocu.view.IView;
 
 public interface IListOfCompleteGameFragment extends IListOfNotCompletedGameFragment {
+
+    void deleteGameFromList();
+
     void onSendGame();
+
     void onAfterAuthUser();
+
     void onErrorSendGame();
+
     void authUser();
 }
