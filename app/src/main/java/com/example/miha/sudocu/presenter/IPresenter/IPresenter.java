0f032b7fa6f099a7a86b2c689a27c4c73e1354bd
miha <vadim.hramov.69@mail.ru>
2017-11-12 23:40:56 +0200

    deleted superfluous

--- app/src/main/java/com/example/miha/sudocu/presenter/IPresenter/IPresenter.java
+++ app/src/main/java/com/example/miha/sudocu/presenter/IPresenter/IPresenter.java
@@ -2,11 +2,8 @@ package com.example.miha.sudocu.presenter.IPresenter;
 
 import android.os.Bundle;
 
-/**
- * Created by miha on 16.11.2016.
- */
 public interface IPresenter {
-    void onSaveInstanceState(Bundle outState);
+    void onSaveInstanceState(Bundle save);
 
     void unSubscription();
 }
