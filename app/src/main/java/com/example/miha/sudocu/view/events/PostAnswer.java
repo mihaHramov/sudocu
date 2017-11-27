c60e2e09d56aa94ad1c4698c58cfef39261fa1a9
miha <vadim.hramov.69@mail.ru>
2017-11-27 11:51:55 +0200

    add event

---
+++ app/src/main/java/com/example/miha/sudocu/view/events/PostAnswer.java
@@ -0,0 +1,13 @@
+package com.example.miha.sudocu.view.events;
+
+public class PostAnswer {
+    private String answer;
+
+    public PostAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
