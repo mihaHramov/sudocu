c2a7dc3c9dda544306eb92e11914de0b1c6948d8
miha <vadim.hramov.69@mail.ru>
2017-11-27 11:22:50 +0200

    add fragment to keyboard

---
+++ app/src/main/java/com/example/miha/sudocu/view/fragment/KeyBoardFragment.java
@@ -0,0 +1,39 @@
+package com.example.miha.sudocu.view.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.miha.sudocu.R;
+import com.example.miha.sudocu.view.IView.IGridView;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class KeyBoardFragment extends Fragment {
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9})
+    void clickOnButton(View v) {
+        String answer = ((Button) v).getText().toString();
+       // presenterGrid.answer(answer);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.list_of_game_saves_fragment, container, false);
+        ButterKnife.bind(rootView);
+        return rootView;
+    }
+}
