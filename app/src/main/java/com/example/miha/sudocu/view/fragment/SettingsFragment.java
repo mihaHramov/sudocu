a4926946084f0117d4069ad615f6b2454463e7af
miha <vadim.hramov.69@mail.ru>
2017-12-02 22:21:49 +0200

    add fragmet for settings

---
+++ app/src/main/java/com/example/miha/sudocu/view/fragment/SettingsFragment.java
@@ -0,0 +1,19 @@
+package com.example.miha.sudocu.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.miha.sudocu.R;
+
+public class SettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_fragment,container,false);
+     //   return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
