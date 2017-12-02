f2bff53762a96e2dd876d50f3e6327da3b7ef2a0
miha <vadim.hramov.69@mail.ru>
2017-12-03 01:30:51 +0200

    use presenter on "SettingsFragment"

--- app/src/main/java/com/example/miha/sudocu/view/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/miha/sudocu/view/fragment/SettingsFragment.java
@@ -6,14 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
+import com.example.miha.sudocu.DP;
 import com.example.miha.sudocu.R;
+import com.example.miha.sudocu.presenter.PresenterSettings;
+import com.example.miha.sudocu.view.IView.ISettingsFragment;
 
-public class SettingsFragment extends Fragment {
+
+public class SettingsFragment extends Fragment implements ISettingsFragment {
+    private PresenterSettings presenterSettings;
+
+    private Switch switchKeyboard;
+
+    @Override
+    public void setKeyboardMod(Boolean flag) {
+        switchKeyboard.setChecked(flag);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_fragment,container,false);
-     //   return super.onCreateView(inflater, container, savedInstanceState);
+        presenterSettings = DP.get().getPresenterSettings();
+        View v = inflater.inflate(R.layout.settings_fragment, container, false);
+        switchKeyboard = (Switch) v.findViewById(R.id.switch_keyboard_mode);
+        switchKeyboard.setOnCheckedChangeListener((buttonView, isChecked) -> presenterSettings.changeKeyboardMode(isChecked));
+        presenterSettings.init(this);
+        return v;
     }
 }
