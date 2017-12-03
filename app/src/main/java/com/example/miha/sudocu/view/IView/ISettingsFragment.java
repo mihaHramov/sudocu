7235e14016dc397edf567824d1197b9288d1a93c
miha <vadim.hramov.69@mail.ru>
2017-12-03 16:01:18 +0200

    add  methode to view "SettingsFragment"

--- app/src/main/java/com/example/miha/sudocu/view/IView/ISettingsFragment.java
+++ app/src/main/java/com/example/miha/sudocu/view/IView/ISettingsFragment.java
@@ -3,4 +3,12 @@ package com.example.miha.sudocu.view.IView;
 
 public interface ISettingsFragment {
     void setKeyboardMod(Boolean flag);
+
+    void setShowSameAnswerMode(Boolean showSameAnswerMode);
+
+    void setShowCountNumberOnButtonMode(Boolean showCountNumberOnButtonMode);
+
+    void setShowKnowAnswerMode(Boolean knowAnswerMode);
+
+    void setShowErrorMode(Boolean errorMode);
 }
--- app/src/main/java/com/example/miha/sudocu/view/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/miha/sudocu/view/fragment/SettingsFragment.java
@@ -13,15 +13,47 @@ import com.example.miha.sudocu.R;
 import com.example.miha.sudocu.presenter.PresenterSettings;
 import com.example.miha.sudocu.view.IView.ISettingsFragment;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class SettingsFragment extends Fragment implements ISettingsFragment {
     private PresenterSettings presenterSettings;
 
-    private Switch switchKeyboard;
+    @BindView(R.id.switch_keyboard_mode)
+    Switch switchKeyboard;
+    @BindView(R.id.show_error_answer)
+    Switch switchShowError;
+    @BindView(R.id.show_count_number_on_button)
+    Switch switchShowCountNumberOnButton;
+    @BindView(R.id.show_know_answer)
+    Switch switchShowKnowAnswer;
+    @BindView(R.id.show_same_answer)
+    Switch switchShowSameAnswer;
 
     @Override
     public void setKeyboardMod(Boolean flag) {
         switchKeyboard.setChecked(flag);
+    }
+
+    @Override
+    public void setShowSameAnswerMode(Boolean showSameAnswerMode) {
+        switchShowSameAnswer.setChecked(showSameAnswerMode);
+    }
+
+    @Override
+    public void setShowCountNumberOnButtonMode(Boolean showCountNumberOnButtonMode) {
+        switchShowCountNumberOnButton.setChecked(showCountNumberOnButtonMode);
+    }
+
+    @Override
+    public void setShowKnowAnswerMode(Boolean knowAnswerMode) {
+        switchShowKnowAnswer.setChecked(knowAnswerMode);
+    }
+
+    @Override
+    public void setShowErrorMode(Boolean errorMode) {
+        switchShowError.setChecked(errorMode);
     }
 
     @Nullable
@@ -29,8 +61,12 @@ public class SettingsFragment extends Fragment implements ISettingsFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         presenterSettings = DP.get().getPresenterSettings();
         View v = inflater.inflate(R.layout.settings_fragment, container, false);
-        switchKeyboard = (Switch) v.findViewById(R.id.switch_keyboard_mode);
+        ButterKnife.bind(this, v);
         switchKeyboard.setOnCheckedChangeListener((buttonView, isChecked) -> presenterSettings.changeKeyboardMode(isChecked));
+        switchShowError.setOnCheckedChangeListener((buttonView, isChecked) -> presenterSettings.changeShowErrorMode(isChecked));
+        switchShowKnowAnswer.setOnCheckedChangeListener((buttonView, isChecked) -> presenterSettings.changeShowKnowAnswerMode(isChecked));
+        switchShowSameAnswer.setOnCheckedChangeListener((buttonView, isChecked) -> presenterSettings.changeShowSameAnswerMode(isChecked));
+        switchShowCountNumberOnButton.setOnCheckedChangeListener((buttonView, isChecked) -> presenterSettings.changeShowCountNumberOnButtonMode(isChecked));
         presenterSettings.init(this);
         return v;
     }
