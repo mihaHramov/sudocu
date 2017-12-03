5bf93711f38151148b3d67c449932d21f4045f2e
miha <vadim.hramov.69@mail.ru>
2017-12-03 16:00:04 +0200

    use methode to presenterSettings

--- app/src/main/java/com/example/miha/sudocu/presenter/PresenterSettings.java
+++ app/src/main/java/com/example/miha/sudocu/presenter/PresenterSettings.java
@@ -19,5 +19,25 @@ public class PresenterSettings {
     public void init(ISettingsFragment fr) {
         view = fr;
         fr.setKeyboardMod(settings.getKeyboardMode());
+        fr.setShowSameAnswerMode(settings.getShowSameAnswerMode());
+        fr.setShowCountNumberOnButtonMode(settings.getShowCountNumberOnButtonMode());
+        fr.setShowKnowAnswerMode(settings.getKnowAnswerMode());
+        fr.setShowErrorMode(settings.getErrorMode());
+    }
+
+    public void changeShowSameAnswerMode(boolean isChecked) {
+        settings.changeShowSameAnswerMode(isChecked);
+    }
+
+    public void changeShowCountNumberOnButtonMode(boolean isChecked) {
+        settings.changeShowCountNumberOnButtonMode(isChecked);
+    }
+
+    public void changeShowKnowAnswerMode(boolean isChecked) {
+        settings.changeShowKnowAnswerMode(isChecked);
+    }
+
+    public void changeShowErrorMode(boolean isChecked) {
+        settings.changeShowErrorMode(isChecked);
     }
 }
