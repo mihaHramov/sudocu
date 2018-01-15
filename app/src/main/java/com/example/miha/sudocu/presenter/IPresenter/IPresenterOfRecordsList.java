a2b0e17b094b73da5cbae91e7f458693893b0951
miha <vadim.hramov.69@mail.ru>
2018-01-15 20:27:17 +0200

    new interface "IPresenterOfRecordsList"

---
+++ app/src/main/java/com/example/miha/sudocu/presenter/IPresenter/IPresenterOfRecordsList.java
@@ -0,0 +1,8 @@
+package com.example.miha.sudocu.presenter.IPresenter;
+
+
+import com.example.miha.sudocu.data.model.Challenge;
+
+public interface IPresenterOfRecordsList extends IPresenterOfFragment {
+    void choiceChallenge(Challenge challenge);
+}
