a893000ed8ae159b77047fe74101d481f26b63a0
miha <vadim.hramov.69@mail.ru>
2018-01-15 19:34:30 +0200

    add new interface "ChallengeItemClickListener"

---
+++ app/src/main/java/com/example/miha/sudocu/view/intf/ChallengeItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.miha.sudocu.view.intf;
+
+import com.example.miha.sudocu.data.model.Challenge;
+
+public interface ChallengeItemClickListener {
+    void onItemClick(Challenge challenge);
+}
