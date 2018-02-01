16d91b30320c3734f6aca86a8f5e120463b73545
miha <vadim.hramov.69@mail.ru>
2018-02-01 20:03:40 +0200

    delete not use interface

--- app/src/main/java/com/example/miha/sudocu/view/intf/ChallengeItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.miha.sudocu.view.intf;
-
-import com.example.miha.sudocu.data.model.Challenge;
-
-public interface ChallengeItemClickListener {
-    void onItemClick(Challenge challenge);
-}
