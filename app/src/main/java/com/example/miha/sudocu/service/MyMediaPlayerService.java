6a573d9cb7c1d943b7fe304790f618afa9f70c48
miha <vadim.hramov.69@mail.ru>
2018-02-20 22:11:38 +0200

    change servise

--- app/src/main/java/com/example/miha/sudocu/service/MyMediaPlayerService.java
+++ app/src/main/java/com/example/miha/sudocu/service/MyMediaPlayerService.java
@@ -6,13 +6,13 @@ import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
-import com.example.miha.sudocu.mvp.view.activity.MainActivity;
 import com.example.miha.sudocu.mvp.view.events.BusProvider;
 import com.example.miha.sudocu.mvp.view.events.OnStopMusicEvent;
 import com.squareup.otto.Bus;
 
 
 public class MyMediaPlayerService extends Service {
+    public static final String name = "name";
     private Bus bus;
     private MediaPlayer player = null;
     @Override
@@ -30,7 +30,7 @@ public class MyMediaPlayerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        int resultMelody = intent.getIntExtra(MainActivity.myMediaPlayer, 0);
+        int resultMelody = intent.getIntExtra(name, 0);
         if(player!=null){
             player.release();//освободил
         }
