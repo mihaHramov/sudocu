81f335bb4ff9270d8c6eae6965d563d216512c19
miha <vadim.hramov.69@mail.ru>
2018-02-20 22:58:04 +0200

    change init "PresenterMainActivity"

--- app/src/main/java/com/example/miha/sudocu/di/component/PlayingActivityComponent.java
+++ app/src/main/java/com/example/miha/sudocu/di/component/PlayingActivityComponent.java
@@ -1,11 +1,14 @@
 package com.example.miha.sudocu.di.component;
 
 
+import com.example.miha.sudocu.di.module.PlayingActivityModule;
+import com.example.miha.sudocu.mvp.presenter.PresenterMainActivity;
 import com.example.miha.sudocu.mvp.view.activity.MainActivity;
 
 import dagger.Subcomponent;
 
-@Subcomponent
+@Subcomponent(modules = PlayingActivityModule.class)
 public interface PlayingActivityComponent {
+    PresenterMainActivity providePresenterMainActivity();
     void inject(MainActivity mainActivity);
 }
--- app/src/main/java/com/example/miha/sudocu/di/module/PlayingActivityModule.java
+++ app/src/main/java/com/example/miha/sudocu/di/module/PlayingActivityModule.java
@@ -4,13 +4,20 @@ package com.example.miha.sudocu.di.module;
 import com.example.miha.sudocu.mvp.data.DP.intf.IRepositorySettings;
 import com.example.miha.sudocu.mvp.presenter.PresenterMainActivity;
 
+import javax.inject.Named;
+
 import dagger.Module;
 import dagger.Provides;
+import rx.Scheduler;
 
 @Module
 public class PlayingActivityModule {
     @Provides
-    PresenterMainActivity providePresenterMainActivity(IRepositorySettings repositorySettings) {
-        return new PresenterMainActivity(repositorySettings);
+    PresenterMainActivity providePresenterMainActivity(IRepositorySettings repositorySettings,
+                                                       Scheduler db,
+                                                       @Named("main") Scheduler main) {
+        PresenterMainActivity mainActivityPresenter = new PresenterMainActivity(repositorySettings);
+        mainActivityPresenter.setSchedulers(db, main);
+        return mainActivityPresenter;
     }
 }
