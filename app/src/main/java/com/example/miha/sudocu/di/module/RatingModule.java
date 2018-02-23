1908b31865834cc91b5a54785bef579be18884ad
miha <vadim.hramov.69@mail.ru>
2018-02-23 15:06:25 +0200

    use RepositoryGame to save game from challenge

--- app/src/main/java/com/example/miha/sudocu/di/module/RatingModule.java
+++ app/src/main/java/com/example/miha/sudocu/di/module/RatingModule.java
@@ -2,6 +2,7 @@ package com.example.miha.sudocu.di.module;
 
 import com.example.miha.sudocu.R;
 import com.example.miha.sudocu.mvp.data.DP.intf.ChallengeDP;
+import com.example.miha.sudocu.mvp.data.DP.intf.IRepositoryGame;
 import com.example.miha.sudocu.mvp.presenter.PresenterRecordsListFragment;
 import com.example.miha.sudocu.mvp.view.adapter.AdapterChallenge;
 
@@ -11,8 +12,8 @@ import dagger.Provides;
 @Module
 public class RatingModule {
     @Provides
-    public PresenterRecordsListFragment providePresenterRecordsListFragment(ChallengeDP dp) {
-        return new PresenterRecordsListFragment(dp);
+    public PresenterRecordsListFragment providePresenterRecordsListFragment(ChallengeDP dp, IRepositoryGame repositoryGame) {
+        return new PresenterRecordsListFragment(dp,repositoryGame);
     }
 
     @Provides
--- app/src/main/java/com/example/miha/sudocu/mvp/presenter/PresenterRecordsListFragment.java
+++ app/src/main/java/com/example/miha/sudocu/mvp/presenter/PresenterRecordsListFragment.java
@@ -5,12 +5,11 @@ import android.util.Log;
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 import com.example.miha.sudocu.mvp.data.DP.intf.ChallengeDP;
+import com.example.miha.sudocu.mvp.data.DP.intf.IRepositoryGame;
 import com.example.miha.sudocu.mvp.data.model.Challenge;
 import com.example.miha.sudocu.mvp.presenter.IPresenter.IPresenterOfRecordsList;
 import com.example.miha.sudocu.mvp.view.intf.IRecordsList;
 
-
-import javax.inject.Inject;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -18,21 +17,38 @@ import rx.schedulers.Schedulers;
 @InjectViewState
 public class PresenterRecordsListFragment extends MvpPresenter<IRecordsList> implements IPresenterOfRecordsList {
     private ChallengeDP challengeDP;
+    public IRepositoryGame repositoryGame;
 
-    @Inject
-    public PresenterRecordsListFragment(ChallengeDP dp) {
+
+    public PresenterRecordsListFragment(ChallengeDP dp, IRepositoryGame repository) {
         this.challengeDP = dp;
+        this.repositoryGame = repository;
     }
 
     @Override
     public void choiceChallenge(Challenge challenge) {
+        challenge.getGrid().setId(0);
+        challenge.getGrid().setUndefined(1);
+        challenge.getGrid().replayGame();
+
+        repositoryGame.saveGame(challenge.getGrid())
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(integer ->saveChallenge(challenge.getLogin(),integer));
+        /*
+        добавить вызов в Бд
+        добавить вызов в таблицу сформировать запис имя + id записи игры
+        * */
         getViewState().choiceChallenge(challenge);
+    }
+
+    private void saveChallenge(String login, Integer id) {
+//        repositoryGame.saveChallenge(login, id);
     }
 
     @Override
     public void onResume() {
         challengeDP.getAllScore()
-                .cache()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(challenges -> getViewState().showRecords(challenges), throwable -> Log.d("mihaHramov", throwable.getMessage()));
