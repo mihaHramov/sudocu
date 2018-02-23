6f9ce4e854a8bd4cbd02dabb7852dd90526b79a4
miha <vadim.hramov.69@mail.ru>
2018-02-23 14:06:46 +0200

    extends MvpView

--- app/src/main/java/com/example/miha/sudocu/mvp/view/intf/IRecordsList.java
+++ app/src/main/java/com/example/miha/sudocu/mvp/view/intf/IRecordsList.java
@@ -1,11 +1,12 @@
 package com.example.miha.sudocu.mvp.view.intf;
 
 
+import com.arellomobile.mvp.MvpView;
 import com.example.miha.sudocu.mvp.data.model.Challenge;
 
 import java.util.ArrayList;
 
-public interface IRecordsList {
+public interface IRecordsList extends MvpView {
     void choiceChallenge(Challenge challenge);
     void showRecords(ArrayList<Challenge> challenges);
 }
