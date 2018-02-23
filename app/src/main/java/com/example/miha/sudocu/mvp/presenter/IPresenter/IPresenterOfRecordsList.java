f21210360309a562c5e18871a955c53ae51a8b1e
miha <vadim.hramov.69@mail.ru>
2018-02-23 14:08:45 +0200

    not extends IPresenterOfFragment and use moxy MvpPresenter

--- app/src/main/java/com/example/miha/sudocu/mvp/presenter/IPresenter/IPresenterOfRecordsList.java
+++ app/src/main/java/com/example/miha/sudocu/mvp/presenter/IPresenter/IPresenterOfRecordsList.java
@@ -2,9 +2,8 @@ package com.example.miha.sudocu.mvp.presenter.IPresenter;
 
 
 import com.example.miha.sudocu.mvp.data.model.Challenge;
-import com.example.miha.sudocu.mvp.view.intf.IRecordsList;
 
-public interface IPresenterOfRecordsList extends IPresenterOfFragment {
+public interface IPresenterOfRecordsList {
+    void onResume();
     void choiceChallenge(Challenge challenge);
-    void setView(IRecordsList recordsList);
 }
--- app/src/main/java/com/example/miha/sudocu/mvp/presenter/PresenterRecordsListFragment.java
+++ app/src/main/java/com/example/miha/sudocu/mvp/presenter/PresenterRecordsListFragment.java
@@ -1,8 +1,9 @@
 package com.example.miha.sudocu.mvp.presenter;
 
-import android.os.Bundle;
 import android.util.Log;
 
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
 import com.example.miha.sudocu.mvp.data.DP.intf.ChallengeDP;
 import com.example.miha.sudocu.mvp.data.model.Challenge;
 import com.example.miha.sudocu.mvp.presenter.IPresenter.IPresenterOfRecordsList;
@@ -14,41 +15,26 @@ import javax.inject.Inject;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-
-public class PresenterRecordsListFragment implements IPresenterOfRecordsList {
+@InjectViewState
+public class PresenterRecordsListFragment extends MvpPresenter<IRecordsList> implements IPresenterOfRecordsList {
     private ChallengeDP challengeDP;
-    private IRecordsList view;
 
     @Inject
     public PresenterRecordsListFragment(ChallengeDP dp) {
-        this.challengeDP =  dp;
-    }
-
-    @Override
-    public void setView(IRecordsList recordsList) {
-        view = recordsList;
+        this.challengeDP = dp;
     }
 
     @Override
     public void choiceChallenge(Challenge challenge) {
-        view.choiceChallenge(challenge);
-    }
-
-    @Override
-    public void init(Bundle bundle) {
-
-    }
-
-    @Override
-    public void savePresenterData(Bundle bundle) {
-
+        getViewState().choiceChallenge(challenge);
     }
 
     @Override
     public void onResume() {
         challengeDP.getAllScore()
+                .cache()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(challenges -> view.showRecords(challenges), throwable -> Log.d("mihaHramov", throwable.getMessage()));
+                .subscribe(challenges -> getViewState().showRecords(challenges), throwable -> Log.d("mihaHramov", throwable.getMessage()));
     }
 }
