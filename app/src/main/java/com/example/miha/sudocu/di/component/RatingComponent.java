f57b33a96b2bf7b54ebb9cf87d54684dc837a28e
miha <vadim.hramov.69@mail.ru>
2018-02-23 14:05:37 +0200

    add getter to presenter

--- app/src/main/java/com/example/miha/sudocu/di/component/RatingComponent.java
+++ app/src/main/java/com/example/miha/sudocu/di/component/RatingComponent.java
@@ -1,6 +1,7 @@
 package com.example.miha.sudocu.di.component;
 
 import com.example.miha.sudocu.di.module.RatingModule;
+import com.example.miha.sudocu.mvp.presenter.PresenterRecordsListFragment;
 import com.example.miha.sudocu.mvp.view.fragment.RecordsListFragment;
 
 import dagger.Subcomponent;
@@ -8,4 +9,5 @@ import dagger.Subcomponent;
 @Subcomponent(modules = {RatingModule.class})
 public interface RatingComponent {
     void inject(RecordsListFragment fragment);
+    PresenterRecordsListFragment getPresenter();
 }
