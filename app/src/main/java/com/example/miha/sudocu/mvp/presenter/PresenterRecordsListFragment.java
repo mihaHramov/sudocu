3d726861cf2aaade402cf62e0e306975c86e5b0d
miha <vadim.hramov.69@mail.ru>
2018-03-19 00:59:53 +0200

    use "getAllLocalChallenge"

--- app/src/main/java/com/example/miha/sudocu/mvp/presenter/PresenterRecordsListFragment.java
+++ app/src/main/java/com/example/miha/sudocu/mvp/presenter/PresenterRecordsListFragment.java
@@ -1,15 +1,16 @@
 package com.example.miha.sudocu.mvp.presenter;
 
-import android.util.Log;
-
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 import com.example.miha.sudocu.mvp.data.DP.intf.ChallengeDP;
 import com.example.miha.sudocu.mvp.data.DP.intf.IRepositoryGame;
+import com.example.miha.sudocu.mvp.data.model.Challenge;
 import com.example.miha.sudocu.mvp.data.model.LocalChallenge;
 import com.example.miha.sudocu.mvp.presenter.IPresenter.IPresenterOfRecordsList;
 import com.example.miha.sudocu.mvp.view.intf.IRecordsList;
 
+
+import java.util.ArrayList;
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -17,13 +18,14 @@ import rx.schedulers.Schedulers;
 
 @InjectViewState
 public class PresenterRecordsListFragment extends MvpPresenter<IRecordsList> implements IPresenterOfRecordsList {
+    private ArrayList<Challenge> localCachChallenge;
     private ChallengeDP challengeDP;
     public IRepositoryGame repositoryGame;
 
 
     public PresenterRecordsListFragment(ChallengeDP dp, IRepositoryGame repository) {
-        this.challengeDP = dp;
         this.repositoryGame = repository;
+        this.challengeDP = dp;
     }
 
     @Override
@@ -37,28 +39,35 @@ public class PresenterRecordsListFragment extends MvpPresenter<IRecordsList> imp
                     .observeOn(AndroidSchedulers.mainThread())
                     .flatMap(integer -> repositoryGame.saveChallenge(localChallenge.getChallenge().getLogin(), integer))
                     .subscribe(integer -> getViewState().choiceChallenge(localChallenge.getChallenge().getGrid()));
-        }else {
-            if(localChallenge.getLocalGame().isGameOver()){
+        } else {
+            if (localChallenge.getLocalGame().isGameOver()) {
                 getViewState().dontStartCompleteGame();
-            }else {
+            } else {
                 getViewState().choiceChallenge(localChallenge.getLocalGame());
             }
         }
     }
 
-
     @Override
     public void onResume() {
-        challengeDP
-                .getAllScore()
+        Observable.concat(Observable.just(localCachChallenge), challengeDP.getAllScore())
+                .filter(challenges -> challenges!=null)
+                .filter(challenges -> !challenges.isEmpty())
+                .first()
+                .doOnNext(challenges -> localCachChallenge = challenges)//local save
                 .subscribeOn(Schedulers.io())
                 .flatMap(Observable::from)
                 .observeOn(Schedulers.newThread())//переключил поток(все что ниже выплняется в другом потоке)
                 .flatMap(challenges -> repositoryGame.getLocalChallenge(challenges))
-                .observeOn(AndroidSchedulers.mainThread())
                 .toList()
-                .doOnCompleted(() -> getViewState().showLoading(false))
+                .onErrorResumeNext(repositoryGame.getAllLocalChallenge())//load from bd when error
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnNext(localChallenges -> {
+                    if (localChallenges.isEmpty()) {
+                        //show error when empty list
+                    }
+                })
                 .doOnSubscribe(() -> getViewState().showLoading(true))
-                .subscribe(challenges -> getViewState().showRecords(challenges), throwable -> Log.d("mihaHramov", throwable.getMessage()));
+                .subscribe(challenges -> getViewState().showRecords(challenges), throwable -> getViewState().showLoading(false), () ->getViewState().showLoading(false));
     }
 }
