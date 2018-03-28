6eb3acf278c6bd9598a8f9a7bf38bcb71fea6afd
miha <vadim.hramov.69@mail.ru>
2018-03-28 12:15:59 +0300

    delete not use classes

--- app/src/main/java/com/example/miha/sudocu/di/module/GameModule.java
+++ app/src/main/java/com/example/miha/sudocu/di/module/GameModule.java
@@ -3,7 +3,6 @@ package com.example.miha.sudocu.di.module;
 import com.example.miha.sudocu.mvp.data.DP.intf.IRepositorySettings;
 import com.example.miha.sudocu.mvp.presenter.IPresenter.IPresenterGrid;
 import com.example.miha.sudocu.mvp.presenter.PresenterGrid;
-import com.example.miha.sudocu.mvp.presenter.PresenterKeyboard;
 import com.example.miha.sudocu.mvp.presenter.interactor.impl.PresenterGridInteractor;
 import com.example.miha.sudocu.mvp.presenter.interactor.intf.IPresenterGridInteractor;
 
@@ -14,15 +13,12 @@ import dagger.Provides;
 @Module
 public class GameModule {
     @Provides
-    public IPresenterGridInteractor providePresenterGridInteractor(IRepositorySettings settings){
+    public IPresenterGridInteractor providePresenterGridInteractor(IRepositorySettings settings) {
         return new PresenterGridInteractor(settings);
     }
+
     @Provides
-    public PresenterKeyboard providePresenterKeyboard(IRepositorySettings settings){
-       return new PresenterKeyboard(settings);
-    }
-    @Provides
-    public IPresenterGrid providePresenterOfGrid(IPresenterGridInteractor interactor){
-     return   new PresenterGrid(interactor);
+    public IPresenterGrid providePresenterOfGrid(IPresenterGridInteractor interactor) {
+        return new PresenterGrid(interactor);
     }
 }
