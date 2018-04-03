e8e3bb87b9737643cc6a7b36e3f35ef5a87938d8
miha <vadim.hramov.69@mail.ru>
2018-04-04 01:19:35 +0300

    add new class to save constant

---
+++ app/src/main/java/com/example/miha/sudocu/mvp/view/constant/BackgroundOfField.java
@@ -0,0 +1,11 @@
+package com.example.miha.sudocu.mvp.view.constant;
+
+
+import com.example.miha.sudocu.R;
+
+public class BackgroundOfField {
+    public static final Integer SameAnswer = R.drawable.show_same_answer;
+    public static final Integer KnowAnswer = R.drawable.show_all_know_answer;
+    public static final Integer ErrorAnswer = R.drawable.show_error_answer;
+    public static final Integer ErrorKnowAnswer = R.drawable.show_error_field;
+}
