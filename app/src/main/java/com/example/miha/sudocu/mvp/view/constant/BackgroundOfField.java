eb9770acf60582d5dd08742547dfaeb31aec9a7d
miha <vadim.hramov.69@mail.ru>
2018-04-04 11:54:15 +0300

    use new Background

--- app/src/main/java/com/example/miha/sudocu/mvp/view/constant/BackgroundOfField.java
+++ app/src/main/java/com/example/miha/sudocu/mvp/view/constant/BackgroundOfField.java
@@ -4,6 +4,10 @@ package com.example.miha.sudocu.mvp.view.constant;
 import com.example.miha.sudocu.R;
 
 public class BackgroundOfField {
+    public static final Integer Focus = R.drawable.focus;
+    public static final Integer ErrorAnswerFocus = R.drawable.focus_error_not_know;
+    public static final Integer ErrorKnowAnswerFocus = R.drawable.focus_error_know_answer;
+    public static final Integer EmptyAnswer = R.drawable.background_grey;
     public static final Integer SameAnswer = R.drawable.show_same_answer;
     public static final Integer KnowAnswer = R.drawable.show_all_know_answer;
     public static final Integer ErrorAnswer = R.drawable.show_error_answer;
